223c83d9957decc3df91821b47249ae1c8772d86
thiagoperest <thiago_teodoroperes07@hotmail.com>
2026-03-23 18:35:00 -0300

    feat: add tarefa 02

--- src/components/ClubeLista.jsx
+++ src/components/ClubeLista.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react'
+
 export default function ClubeLista() {
+  const [clubes, setClubes] = useState([])
+
+  useEffect(() => {
+    fetch('/src/utils/db/clubes.json')
+      .then(response => response.json())
+      .then(data => setClubes(data))
+      .catch(error => console.error('Erro ao carregar clubes:', error))
+  }, [])
+
   return (
     <div>
       <h1>Clubes de Leitura</h1>
       <ul>
+        {clubes.map(clube => (
+          <li key={clube.id}>{clube.nome}</li>
+        ))}
       </ul>
     </div>
   )
